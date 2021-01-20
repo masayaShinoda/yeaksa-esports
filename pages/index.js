d856408a56d547703643e495862b7f05aea73eda
Masaya Shida <masayashida@gmail.com>
2021-01-20 23:36:56 +0700

    INITIAL COMMIT

--- pages/index.js
+++ pages/index.js
@@ -6,20 +6,35 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>YeakSa E-sports</title>
-        <link rel="icon" href="/favicon.ico" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       </Head>
+
+      <div className={styles.background}></div>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Coming soon
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Pioneers in Cambodian esports.
         </p>
 
-        <div className={styles.grid}>
+        <span>
+            <a href="https://www.facebook.com/yeaksa.official">
+              <img src="/svg/fb.svg" alt="Fan Sport" className={styles.logo} style={{margin: `0 0.75vmax`}} />
+            </a>
+            <a href="https://www.instagram.com/yeaksa.esports/">
+              <img src="/svg/ig.svg" alt="Good Time Group" className={styles.logo} style={{margin: `0 0.75vmax`}} />
+            </a>
+            <a href="https://www.youtube.com/channel/UCXeZPlbx0ncbnYijLtFkZgQ">
+              <img src="/svg/yt.svg" alt="Bees FC" className={styles.logo} style={{margin: `0 0.75vmax`}} />
+            </a>
+          </span>
+
+        {/* <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
@@ -47,18 +62,24 @@ export default function Home() {
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
-        </div>
+        </div> */}
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+      <footer style={{marginBottom: `5vh`}}className={styles.footer}>
+          <p style={{marginRight: `.5vmax`}}>
+            Proudly sponsored by
+          </p>
+          <span>
+            <a href="https://www.facebook.com/fansportstore">
+              <img src="/sponsors/Logo-Fan-Sport-png - Copy.png" alt="Fan Sport" className={styles.logo} />
+            </a>
+            <a href="https://www.facebook.com/Good-Time-Hotel-Resort-Group-106046934410922">
+              <img src="/sponsors/GoodTimeGroup_White_Logo.png" alt="Good Time Group" className={styles.logo} />
+            </a>
+            <a href="https://www.facebook.com/BEES-FC-111744477257357">
+              <img src="/sponsors/bees-logo.png" alt="Bees FC" className={styles.logo} />
+            </a>
+          </span>
       </footer>
     </div>
   )
