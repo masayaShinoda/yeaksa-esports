b7a966eff3592f5e811ccc91703a6a4ea5957818
Masaya Shida <masayashida@gmail.com>
2021-02-02 00:42:06 +0700

    test progress bar implementation

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import NextNprogress from 'nextjs-progressbar'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <NextNprogress
+        color="#29D"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height="3"
+      />
+    </>
+  )
 }
 
 export default MyApp
