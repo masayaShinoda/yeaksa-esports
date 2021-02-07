f23b2f1bd9670f996d8e8f09b9c961194c96cb24
Masaya Shida <masayashida@gmail.com>
2021-02-07 22:57:33 +0700

    frontpage content now served dynamically

---
+++ components/content.js
@@ -0,0 +1,91 @@
+import { useState, useEffect } from "react";
+import ReactPlayer from "react-player"
+import contentStyles from "../styles/content.module.css"
+
+export default function Content() {
+    const token = '7d75f2d5a1e12e28d4ee89f229cdc5';
+    const [homeData, setHomeData] = useState((homeData) => {return null}); // by default there is no homepage data
+    
+    useEffect(() => {
+        fetch(
+            'https://graphql.datocms.com/',
+            {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${token}`,
+              },
+              body: JSON.stringify({
+                query: `{ 
+                    allHomepages {
+                        content {
+                          ... on HeadingRecord {
+                            id
+                            heading
+                            subheading
+                            date
+                          }
+                          ... on BodyRecord {
+                            id
+                            text
+                            singlephoto {
+                              url
+                              alt
+                            }
+                            video {
+                              url
+                            }
+                          }
+                        }
+                      }
+                  }`
+              }),
+            }
+        )
+        .then(res => res.json())
+        .then((res) => {
+            setHomeData(res.data["allHomepages"])
+            console.log(res.data["allHomepages"])
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }, [])
+    return (
+        <div className={contentStyles.contentContainer}>
+            <link
+                rel="stylesheet"
+                href="https://video-react.github.io/assets/video-react.css"
+            />
+            {homeData &&
+                homeData.map(data => {
+                    return (
+                        <>
+                            <div className={contentStyles.div1}>
+                                <h1>
+                                    {data.content[0].heading}
+                                </h1>
+                                {data.content[0].subheading ? <h2>{data.content[0].subheading}</h2> : null}
+                            </div>
+                            <div className={contentStyles.div2}>
+                                 
+                            
+                                {data.content[1].singlephoto ? <img src={data.content[1].singlephoto.url} alt={data.content[1].singlephoto.url} /> : null}
+
+                                {data.content[1].video ? 
+                                   <ReactPlayer url={data.content[1].video.url} />                                    
+                                : null}
+
+                                <p>
+                                    {data.content[1].text}
+                                </p>
+                            </div>
+
+                        </>
+                    )
+                })
+            }
+        </div>
+    )
+}
--- components/frontpage.js
+++ components/frontpage.js
@@ -1,14 +1,17 @@
+import Content from "./content"
 import styles from "../styles/Frontpage.module.css"
 
 export default function Frontpage() {
     return (
         <div className={styles.frontpageContainer}>
-            <div>
+            <div className={styles.frontpage}>
                 <h1 style={{marginBottom: 0}}>We're back!</h1>
                 <p>Saying hello to the Cambodian E-sports scene, YeakSa is back prepared and up for the challenge.</p>                
                 <a className={styles.actionBtn} href="/roster">See Our Roster &#187;</a>
             </div>
             <iframe style={{marginTop: `2.5vmax`}}width="560" height="315" src="https://www.youtube-nocookie.com/embed/HNbO-oSpdjo?controls=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+   
+            <Content />
         </div>
 
     )
