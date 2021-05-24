6d91388c32c4013f3a511467f325db76ae86185a
masayaShinoda <masayashida@gmail.com>
2021-05-24 16:45:08 +0700

    added darkmode useState to layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,13 +1,54 @@
 import { NextSeo } from 'next-seo';
+import { useState, useEffect } from "react";
 import Head from 'next/head';
 import Nav from "./nav.js"
 import Footer from "./footer"
 import styles from '../styles/Home.module.css'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMoon } from '@fortawesome/free-solid-svg-icons'
+import { faLightbulb } from '@fortawesome/free-solid-svg-icons'
+
+// Check if window is defined (so if in the browser or in node.js).
+const isBrowser = typeof window !== "undefined"
+
 export default function Layout({ children, pageName }) {
+    const [isDarkMode, setDarkMode] = useState(false)
     
+    useEffect(() => {
+        if (isBrowser) {    
+            const darkmodeStatus = localStorage.getItem("darkmode")
+            if (darkmodeStatus === "dark") {
+                setDarkMode(true)        
+            } else {
+                setDarkMode(false)                
+            }
+        }
+    }, [])
+
+    useEffect(() => {       
+
+        if(isDarkMode) {
+            document.getElementById("container").classList.add("dark")
+            localStorage.setItem("darkmode", "dark")
+            // setDarkMode(true)//
+        } else {
+            document.getElementById("container").classList.remove("dark")
+            localStorage.setItem("darkmode", "light")
+            // setDarkMode(false)
+        }
+        // localStorage.setItem("darkmode", isDarkMode)
+
+    }, [isDarkMode])
+
+
+    const goDarkBtn = <button onClick={() => {setDarkMode(!isDarkMode)}} className="goDarkBtn">
+                        <FontAwesomeIcon icon={faMoon} title="Lights off" style={{width: `1em`, maxWidth: `1em`}} />
+                        <FontAwesomeIcon icon={faLightbulb} title="Lights on" style={{width: `1em`, maxWidth: `1em`}} />
+                      </button>
+                          
     return (
-        <div className={styles.container}>
+        <div className={styles.container} id="container">
             <Head>
             <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
             <link rel="icon" href="/favicon.ico" type="image/x-icon" />
