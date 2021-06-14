9b47957399178ce9de908703b86999fbf31c1dd4
Masaya Shida <masayashida@gmail.com>
2021-06-15 00:32:18 +0700

    updated timelinejs

--- components/timeline.js
+++ components/timeline.js
@@ -45,6 +45,41 @@ export default function Timeline() {
     return (
         <TimelineAccordion>
             <ul className={timelineStyles.timeline}>
+                <li>
+                    <div className={timelineStyles.direction_l}>
+                        <div className={timelineStyles.flag_wrapper}>
+                            <span className={timelineStyles.flag}>
+                                Startup
+                            </span>
+                            <span className={timelineStyles.time_wrapper}>
+                                <span className={timelineStyles.time}>
+                                    January 2021
+                                </span>
+                            </span>
+                        </div>
+                        <div className={timelineStyles.desc}>
+                            Yeaksa was formerly a local CS:GO team in 2020. Then we became an esports organization.
+                        </div>
+                    </div>
+                </li>       
+            <li>
+                    <div className={timelineStyles.direction_r}>
+                        <div className={timelineStyles.flag_wrapper}>
+                            <span className={timelineStyles.flag}>
+                                1k likes
+                            </span>
+                            <span className={timelineStyles.time_wrapper}>
+                                <span className={timelineStyles.time}>
+                                    February 2021
+                                </span>
+                            </span>
+                        </div>
+                        <div className={timelineStyles.desc}>
+                            Our first social media milestone.
+                        </div>
+                    </div>
+                </li>
+
             {timelineData && 
                     timelineData.map(data => {
                         if(data.left) {
@@ -91,41 +126,7 @@ export default function Timeline() {
                         }
                     })
                 }
-                <li>
-                    <div className={timelineStyles.direction_r}>
-                        <div className={timelineStyles.flag_wrapper}>
-                            <span className={timelineStyles.flag}>
-                                1k likes
-                            </span>
-                            <span className={timelineStyles.time_wrapper}>
-                                <span className={timelineStyles.time}>
-                                    February 2021
-                                </span>
-                            </span>
-                        </div>
-                        <div className={timelineStyles.desc}>
-                            Our first social media milestone.
-                        </div>
-                    </div>
-                </li>
-
-                <li>
-                    <div className={timelineStyles.direction_l}>
-                        <div className={timelineStyles.flag_wrapper}>
-                            <span className={timelineStyles.flag}>
-                                Startup
-                            </span>
-                            <span className={timelineStyles.time_wrapper}>
-                                <span className={timelineStyles.time}>
-                                    January 2021
-                                </span>
-                            </span>
-                        </div>
-                        <div className={timelineStyles.desc}>
-                            Yeaksa was formerly a local CS:GO team in 2020. Then we became an esports organization.
-                        </div>
-                    </div>
-                </li>                
+         
 
             
 
