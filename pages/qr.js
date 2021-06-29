432acc40f475ac3a12ad057a92753416cf71b368
Masaya Shida <masayashida@gmail.com>
2021-06-29 14:15:10 +0700

    push qr page

---
+++ pages/qr.js
@@ -0,0 +1,38 @@
+import Layout from "../components/layout"
+
+export default function Qr() {
+    function copyToClipboard() {
+        /* Get the text field */
+        var copyText = document.getElementById("passwordText");
+      
+        /* Select the text field */
+        copyText.select();
+        copyText.setSelectionRange(0, 99999); /* For mobile devices */
+      
+        /* Copy the text inside the text field */
+        document.execCommand("copy");
+      
+        /* Alert the copied text */
+        // alert("Copied password " + copyText.value);
+      } 
+
+    return (
+        <Layout pageName="QR">
+            <p>
+                Wifi password:
+            </p>
+                <input type="text" value="yeak@freewifi" id="passwordText" 
+                    style={{
+                        fontSize: `1.8rem`,
+                        margin: `.8rem 0`,
+                        padding: `.25rem .8rem`
+                    }}></input>
+                <button 
+                    onClick={() => copyToClipboard()}
+                    style={{padding: `.5rem .8rem`, fontSize: `1.1rem`}}
+
+                >Copy to clipboard</button>
+            
+        </Layout>
+    )
+}
